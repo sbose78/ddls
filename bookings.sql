55dcaa778aff5b2e69ec23aefb77d10841dfa068
Shoubhik Bose <shbose@redhat.com>
2024-03-18 14:37:32 -0400

    Update bookings.sql

--- bookings.sql
+++ bookings.sql
@@ -1,7 +1,8 @@
 --liquibase formatted sql
 
 --changeset liquibase:1
-CREATE DATABASE IF NOT EXISTS LIQUIBASE_TEST
+CREATE DATABASE IF NOT EXISTS LIQUIBASE_TEST;
+CREATE SCHEMA IF NOT EXISTS LIQUIBASE_TEST.TEST;
   
 --changeset liquibase:2
-CREATE TABLE IF NOT EXISTS test_table (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id))
+CREATE TABLE IF NOT EXISTS test_table (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id));
